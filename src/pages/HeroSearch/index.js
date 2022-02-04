a36c7350310fc0723df6dc0dca8cce63e95abf2b
Carmen Guevara <carmen.guevara75@gmail.com>
2022-02-04 16:06:57 -0500

    Search Styling

--- src/pages/HeroSearch/index.js
+++ src/pages/HeroSearch/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { useContext, useState, useEffect } from 'react'
+import './styles.css'
 
 const HeroSearch = () => {
 
@@ -25,7 +26,7 @@ const HeroSearch = () => {
     // }, [stringSearch])
 
     const searchCharacter = async (stringSearch) => {
- 
+
         try {
             const response = await axios.get(`https://www.superheroapi.com/api.php/10158975138651775/search/${stringSearch}`)
             setFoundCharacter(response.data.results)
@@ -34,11 +35,11 @@ const HeroSearch = () => {
             console.log(error)
         }
     }
-    
+
     return (
 
         <div>
-           
+
             <div className="ui form success">
                 <div className="field">
                     <label>Type the Name of the Character you are looking for</label>
@@ -49,27 +50,31 @@ const HeroSearch = () => {
                     return searchCharacter(stringSearch)
                 }}>Search</button>
             </div>
-             {console.log('inside return',foundCharacter)}
+            {console.log('inside return', foundCharacter)}
             {
-
-                    foundCharacter.map(item => {
-                        return (   
-                            
-                            <div className="card">
-                            <div className="image">
-                              <img src={item.image.url}/>
-                            </div>
-                            <div className="content">
-                              <div className="header">{item.name}</div>
-                              <div className="meta">
-                                <a>Friends</a>
-                              </div>
-
-                            </div>
-
-                          </div>
-                        )
-                    })
+                foundCharacter.map(item => {
+                    return (
+                        <div id='card-container'>
+                        <div className="card hero-card">
+                            <img id= "pict-set"src={item.image.url} className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                    <h5 className="card-title">{item.name}</h5>
+                                    <p className="card-text">{item.work.occupation}</p>
+                                    <div id='detail-container'>
+                                    {
+                                        (item.powerstats.power!="null") ? 
+                                        <button className="btn btn-primary">Add To Cart</button>
+                                        :
+                                        <button className="btn btn-primary" disabled>Sold-Out</button>
+                                        
+                                    }    
+                                    <a href="#" id="price" className="btn btn-primary">Price : ${(item.powerstats.power!="null")? item.powerstats.power : "Sold-Out" }</a>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                    )
+                })
             }
 
         </div>
